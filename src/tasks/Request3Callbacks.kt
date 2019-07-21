a3def447768ea217ec2f5581be59c48ec1f76e7b
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-21 14:16:50 -0300

    Using the reactivex to update users

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -1,28 +1,35 @@
 package tasks
 
 import contributors.*
+import io.reactivex.subjects.PublishSubject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
-        val allUsers = Collections.synchronizedList(mutableListOf<User>())
-        val numberOfProcessed = AtomicInteger(0)
-        for (repo in repos) {
+        val subject = PublishSubject.create<List<User>>()
+        for ((index, repo) in repos.withIndex()) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
-                allUsers += users
-                if (numberOfProcessed.incrementAndGet() == repos.size) {
-                    updateResults(allUsers.aggregate())
-                }
+                subject.onNext(users)
+                if (index == repos.lastIndex) subject.onComplete()
             }
         }
+        subject
+            .reduce(LinkedList(), toListOfUsers())
+            .subscribe { list -> updateResults(list.aggregate()) }
+    }
+}
+
+private fun toListOfUsers(): (LinkedList<User>, List<User>) -> LinkedList<User> {
+    return { accumulator: LinkedList<User>, currentValue: List<User> ->
+        for (user in currentValue) accumulator.push(user)
+        accumulator
     }
 }
 
