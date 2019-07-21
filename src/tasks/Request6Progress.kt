bbdd4dfb528fa2eed67f3da8110c653a9c2d12a2
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-21 20:11:57 -0300

    Update result asynchronous

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,24 @@
 package tasks
 
 import contributors.*
+import java.util.*
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service.getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+    val allUsers = LinkedList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        for (user in users) allUsers.push(user)
+        updateResults(allUsers.aggregate(), index == repos.lastIndex)
+    }
 }
