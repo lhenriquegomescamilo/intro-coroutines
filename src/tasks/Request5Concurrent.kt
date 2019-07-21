8e7799111102513d665df96bcf2cbaa7effb9399
lcamilo <lhenriquegomescamilo@gmail.com>
2019-07-21 18:36:53 -0300

    Working with multithread and concurrency using suspend and async from kotlin
    * Request5Concurrent.kt:
     * Here have the ocurrency all magic

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -1,8 +1,24 @@
 package tasks
 
 import contributors.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repositories = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    val allDeferred = repositories
+        .map { repo ->
+            async(Dispatchers.Default) {
+                service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+            }
+        }
+    allDeferred.awaitAll().flatten().aggregate()
 }
